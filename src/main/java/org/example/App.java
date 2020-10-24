72a4e720d9541d825c38108cf64564eff12f53ad
Richard <d00230925@student.dkit.ie>
2020-10-24 01:32:47 +0100

    Added a method to print neatly.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,77 +7,68 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Hello world!
+ * Richard Collins
+ * D00230925
  */
 public class App {
+
     public static void main(String[] args) {
         App app = new App();
         app.start();
     }
 
     private static void start() {
-
         File fileName = new File("JC_Results.txt");
         readFile(fileName);
-
-
     }
 
     public static void readFile(File file) {
-
+        int[] codes = new int[8];
+        int[] grades = new int[8];
+        String studentID = null;
 
         try {
             Scanner scanner = new Scanner(new File(String.valueOf(file)));
             scanner.useDelimiter("[,\r\n]+");
-
-            String studentID;
-            int[] codes = new int[8];
-            int[] grades = new int[8];
-
-
+            System.out.println("Student Average");
             while (scanner.hasNext()) {
-
-                studentID = scanner.next(); // reads in as String
-
+                studentID = scanner.next();
                 for (int i = 0; i < 8; i++) {
                     codes[i] = scanner.nextInt();
                     grades[i] = scanner.nextInt();
                 }
                 int[] selectedGrades = selectFiveGrades(codes, grades);
-                System.out.println(Arrays.toString(selectedGrades));
                 double avg = calculateAverage(selectedGrades);
-                System.out.println(avg);
 
+                print(studentID, avg);
             }
-
         } catch (IOException e) {
             System.out.println("Error - Couldn't find file");
         }
 
+
     }
 
+    private static void print(String studentID, double avg) {
+        System.out.printf("%s %6.2f\n" , studentID, avg);
+    }
 
     public static int[] selectFiveGrades(int[] codes, int[] grades) {
-
         ArrayList<Integer> bestGrades = new ArrayList<>(0);
         int largestA = grades[0];
         int largestB = -1;
 
         for (int i = 0; i < codes.length; i++) {
-
             if (codes[i] <= 3) {
                 bestGrades.add(grades[i]);
             } else if (codes[i] > 3) {
 
-
                 for (int j = 0; j < grades.length; j++) {
-
                     if (grades[i] > largestA) {
                         largestB = largestA;
                         largestA = grades[i];
                     } else if (grades[i] > largestB && largestA != grades[i]) {
                         largestB = grades[i];
-
                     }
                 }
             }
@@ -90,23 +81,19 @@ public class App {
             result[j] = bestGrades.get(j);
         }
 
-
         return result;
     }
 
     public static double calculateAverage(int[] selectedGrades) {
-
-
         double avg = 0;
-
         int total = 0;
 
         for (int i = 0; i < selectedGrades.length; i++) {
             total = selectedGrades[i] + total;
         }
-
         avg = (double) total / selectedGrades.length;
 
         return avg;
     }
+
 }
