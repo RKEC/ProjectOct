72092bded2a9766f9181f4e861f04f035bedaf34
Richard <d00230925@student.dkit.ie>
2020-10-24 01:51:41 +0100

    Added comment descriptions.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,7 +3,6 @@ package org.example;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -23,14 +22,16 @@ public class App {
     }
 
     public static void readFile(File file) {
+        //declare
         int[] codes = new int[8];
         int[] grades = new int[8];
-        String studentID = null;
+        String studentID;
 
         try {
             Scanner scanner = new Scanner(new File(String.valueOf(file)));
             scanner.useDelimiter("[,\r\n]+");
             System.out.println("Student Average");
+            //runs through data until there is no more in file
             while (scanner.hasNext()) {
                 studentID = scanner.next();
                 for (int i = 0; i < 8; i++) {
@@ -45,24 +46,26 @@ public class App {
         } catch (IOException e) {
             System.out.println("Error - Couldn't find file");
         }
-
-
     }
 
     private static void print(String studentID, double avg) {
+        //prints details neatly in table form
         System.out.printf("%s %6.2f\n" , studentID, avg);
     }
 
     public static int[] selectFiveGrades(int[] codes, int[] grades) {
+        //declare
         ArrayList<Integer> bestGrades = new ArrayList<>(0);
         int largestA = grades[0];
         int largestB = -1;
 
+        //loop that identifies best grades
         for (int i = 0; i < codes.length; i++) {
+            //adds Maths, Irish and English to arr
             if (codes[i] <= 3) {
                 bestGrades.add(grades[i]);
             } else if (codes[i] > 3) {
-
+                //loop that find the highest grades that isn't Maths, Irish or English
                 for (int j = 0; j < grades.length; j++) {
                     if (grades[i] > largestA) {
                         largestB = largestA;
@@ -73,10 +76,12 @@ public class App {
                 }
             }
         }
+        //adds highest grade to arr
         bestGrades.add(largestA);
+        //adds second highest grade to arr
         bestGrades.add(largestB);
-        int[] result = new int[bestGrades.size()];
 
+        int[] result = new int[bestGrades.size()];
         for (int j = 0; j < bestGrades.size(); j++) {
             result[j] = bestGrades.get(j);
         }
@@ -85,12 +90,15 @@ public class App {
     }
 
     public static double calculateAverage(int[] selectedGrades) {
-        double avg = 0;
+        //declare
+        double avg;
         int total = 0;
 
-        for (int i = 0; i < selectedGrades.length; i++) {
-            total = selectedGrades[i] + total;
+        //loop to add grades
+        for(int selectedGrade : selectedGrades) {
+            total = selectedGrade + total;
         }
+        //cal to get avg
         avg = (double) total / selectedGrades.length;
 
         return avg;
